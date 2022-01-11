b81d076376e513de4aafb50ea33e0cbe1189ebf8
siddharthnair04 <74850334+siddharthnair04@users.noreply.github.com>
2022-01-11 11:20:18 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelector('.btn2').style.display = 'none'; 
+document.querySelector('.btn3').style.display = 'none'; 
+document.querySelector('.btn1').addEventListener('click', showBtn); 
+
+function showBtn(e) { 
+    document.querySelector('.btn2').style.display = 'block'; 
+    document.querySelector('.btn3').style.display = 'block'; 
+    e.preventDefault(); 
+   }
